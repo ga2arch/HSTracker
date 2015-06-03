fb5c88b7dcf643562911774f7dd3b108011d3b56
Gabriele Carrettoni <gabriele.carrettoni@gmail.com>
2015-06-03 12:26:12 +0200

    Changed dependendicies

---
+++ target/debug/hearthstone.d
@@ -0,0 +1,4 @@
+/Users/Gabriele/Dropbox/Progetti/rust/hearthstone/target/debug/hearthstone: src/main.rs
+
+/Users/Gabriele/Dropbox/Progetti/rust/hearthstone/target/debug/hearthstone.d: src/main.rs
+
